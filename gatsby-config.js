a55ad69a144a9440f3986e28029c3723035684a3
ALEX LIM <alim@ccs.neu.edu>
2019-07-24 09:56:48 -0400

    Update analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,14 +48,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-81197887-3",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
